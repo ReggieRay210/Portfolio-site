cd46c3711540a7d645a2a3e859f2f3efb449bcce
ReggieRay210 <r.griffin2680@gmail.com>
2026-02-06 13:46:10 -0600

    Update to layout.tsx page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,51 @@
+'use client';
 import './globals.css';
-import type { Metadata } from 'next';
+import type { Metadata }
+    from 'next';
 import { Inter } from 'next/font/google';
+import { useState, useEffect } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
-const layoutColor ='min-h-screen bg-gradient-to-br from-[#2B66F7] from-0% via-[#9A557D] via-50% to-[#FF4303] to-100%'
+const layoutColor = 'min-h-screen bg-gradient-to-br from-[#2B66F7] from-0% via-[#9A557D] via-50% to-[#FF4303] to-100%'
 
-export const metadata: Metadata = {
-  title: 'Reginald Griffin II | Portfolio Page',
-  description: 'Portfolio page',
+const metadata: Metadata = {
+    title: 'Reginald Griffin II | Portfolio Page',
+    description: 'Portfolio page'
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+function ScrollToTop() {
+    const [visible, setVisible] = useState(false);
+
+    useEffect(() => {
+        const onScroll = () => {
+            setVisible(window.scrollY > 50);
+        };
+
+        window.addEventListener('scroll', onScroll);
+        return () => window.removeEventListener('scroll', onScroll);
+    }, []);
+    if (!visible)
+        return null;
+
+    return (
+        <button onClick={
+            () => window.scrollTo({ top: 0, behavior: 'smooth' })
+        }className='fixed z-50' ><img width="50" height="50" src="https://img.icons8.com/ios/50/circled-chevron-up.png" alt="circled-chevron-up" /></button>
+    );
+}
+
+export default function RootLayout({ children }: {
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={`scroll-smooth text-white text-center mx-2 ${layoutColor}${inter.className}`}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={
+                `scroll-smooth text-white text-center mx-2 ${layoutColor}${inter.className
+                }`
+            }>
+                {children}
+                <ScrollToTop />
+            </body>
+        </html>
+    );
 }
